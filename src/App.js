62e0a60aa79bbbfe8bba0925649ca41718b5561c
orisa07037616291 <orisakwepeter@gmail.com>
2022-03-28 20:53:01 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import images_04 from "./assets/images_04.jpg";
+import images_06 from "./assets/images_06.jpg";
+import images_07 from "./assets/images_07.jpg";
+import Hero from "./components/Hero";
+import Slider from "./components/Slider";
+import Navbar from "./components/Navbar";
+
+const navbarLinks = [
+  { url: "#", title: "Home" },
+  { url: "#", title: "Trips" },
+  { url: "#", title: "Reward" },
+];
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar navbarLinks={navbarLinks} />
+      <Hero imageSrc={images_04} />
+
+      <Slider
+        imageSrc={images_06}
+        title={"Be an explorer!"}
+        subtitle={"Our platform offers wide variety of unique travel locations"}
+      />
+
+      <Slider
+        imageSrc={images_07}
+        title={"Memories of a lifetime"}
+        subtitle={"Your dream vacations is only a few clicks away"}
+        flipped={true}
+      />
     </div>
   );
 }
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,46 @@
+import React, { useState } from "react";
+import "./Navbar.css";
+import { FiMenu, FiX } from "react-icons/fi";
+
+const navbarLinks = [
+  { url: "#", title: "Home" },
+  { url: "#", title: "Trips" },
+  { url: "#", title: "Rewards" },
+];
+
+const Navbar = ({ navbarLinks }) => {
+  const [menuClicked, setMenuClicked] = useState(false);
+  const toggleMenuClick = () => {
+    setMenuClicked(!menuClicked);
+  };
+
+  return (
+    <nav className="navbar">
+      <span className="navbar__logo">Eden's Ticket</span>
+
+      {menuClicked ? (
+        <FiMenu size={25} className="navbar__menu" onClick={toggleMenuClick} />
+      ) : (
+        <FiX size={25} className="navbar__menu" onClick={toggleMenuClick} />
+      )}
+
+      <ul
+        className={
+          menuClicked ? "navbar__list" : "navbar__list navbar__list--active"
+        }
+      >
+        {navbarLinks.map((item) => {
+          return (
+            <li className="navbar__item" key={item.title}>
+              <a className="navbar__link" href={item.url}>
+                {item.title}
+              </a>
+            </li>
+          );
+        })}
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
